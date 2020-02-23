6c2c64b5a039b12cc79c41e8fbfebd6d53f97660
linhnguyen99 <linhmt100899@gmail.com>
2020-02-24 02:54:34 +0700

    product_app
    product_app

---
+++ product_app/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'product/index'
+
+  root 'product#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
